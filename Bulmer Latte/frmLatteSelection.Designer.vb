e1b3381c9156418aebb74dafd9af5d13fd63b15a
Mark David Bulmer <mdbulmer86@gmail.com>
2023-04-30 18:06:45 -0400

    Add files via upload

---
+++ Bulmer Latte/frmLatteSelection.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLatteSelection
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblConfirmation = New System.Windows.Forms.Label()
+        Me.lblInstructions = New System.Windows.Forms.Label()
+        Me.btnMocha = New System.Windows.Forms.Button()
+        Me.btnSelect = New System.Windows.Forms.Button()
+        Me.btnPumpkin = New System.Windows.Forms.Button()
+        Me.picMocha = New System.Windows.Forms.PictureBox()
+        Me.picPumpkin = New System.Windows.Forms.PictureBox()
+        Me.lblHeading = New System.Windows.Forms.Label()
+        CType(Me.picMocha, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picPumpkin, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.Bisque
+        Me.btnExit.Enabled = False
+        Me.btnExit.Location = New System.Drawing.Point(289, 493)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(165, 25)
+        Me.btnExit.TabIndex = 17
+        Me.btnExit.Text = "Exit Window"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'lblConfirmation
+        '
+        Me.lblConfirmation.AutoSize = True
+        Me.lblConfirmation.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblConfirmation.Location = New System.Drawing.Point(296, 467)
+        Me.lblConfirmation.Name = "lblConfirmation"
+        Me.lblConfirmation.Size = New System.Drawing.Size(150, 14)
+        Me.lblConfirmation.TabIndex = 16
+        Me.lblConfirmation.Text = "Enjoy your latte selection."
+        '
+        'lblInstructions
+        '
+        Me.lblInstructions.AutoSize = True
+        Me.lblInstructions.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblInstructions.Location = New System.Drawing.Point(213, 431)
+        Me.lblInstructions.Name = "lblInstructions"
+        Me.lblInstructions.Size = New System.Drawing.Size(316, 14)
+        Me.lblInstructions.TabIndex = 15
+        Me.lblInstructions.Text = "Choose a latte flavor and then click Select Latte button."
+        '
+        'btnMocha
+        '
+        Me.btnMocha.BackColor = System.Drawing.Color.Bisque
+        Me.btnMocha.Location = New System.Drawing.Point(530, 386)
+        Me.btnMocha.Name = "btnMocha"
+        Me.btnMocha.Size = New System.Drawing.Size(165, 25)
+        Me.btnMocha.TabIndex = 14
+        Me.btnMocha.Text = "Mocha"
+        Me.btnMocha.UseVisualStyleBackColor = False
+        '
+        'btnSelect
+        '
+        Me.btnSelect.BackColor = System.Drawing.Color.Bisque
+        Me.btnSelect.Enabled = False
+        Me.btnSelect.Location = New System.Drawing.Point(289, 386)
+        Me.btnSelect.Name = "btnSelect"
+        Me.btnSelect.Size = New System.Drawing.Size(165, 25)
+        Me.btnSelect.TabIndex = 13
+        Me.btnSelect.Text = "Select Latte"
+        Me.btnSelect.UseVisualStyleBackColor = False
+        '
+        'btnPumpkin
+        '
+        Me.btnPumpkin.BackColor = System.Drawing.Color.Bisque
+        Me.btnPumpkin.Location = New System.Drawing.Point(48, 386)
+        Me.btnPumpkin.Name = "btnPumpkin"
+        Me.btnPumpkin.Size = New System.Drawing.Size(165, 25)
+        Me.btnPumpkin.TabIndex = 12
+        Me.btnPumpkin.Text = "Pumpkin Spice"
+        Me.btnPumpkin.UseVisualStyleBackColor = False
+        '
+        'picMocha
+        '
+        Me.picMocha.Image = Global.Bulmer_Latte.My.Resources.Resources.mocha
+        Me.picMocha.Location = New System.Drawing.Point(395, 86)
+        Me.picMocha.Name = "picMocha"
+        Me.picMocha.Size = New System.Drawing.Size(300, 250)
+        Me.picMocha.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picMocha.TabIndex = 11
+        Me.picMocha.TabStop = False
+        Me.picMocha.Visible = False
+        '
+        'picPumpkin
+        '
+        Me.picPumpkin.Image = Global.Bulmer_Latte.My.Resources.Resources.pumpkin
+        Me.picPumpkin.Location = New System.Drawing.Point(48, 86)
+        Me.picPumpkin.Name = "picPumpkin"
+        Me.picPumpkin.Size = New System.Drawing.Size(300, 250)
+        Me.picPumpkin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picPumpkin.TabIndex = 10
+        Me.picPumpkin.TabStop = False
+        Me.picPumpkin.Visible = False
+        '
+        'lblHeading
+        '
+        Me.lblHeading.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Font = New System.Drawing.Font("Tahoma", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeading.ForeColor = System.Drawing.Color.SaddleBrown
+        Me.lblHeading.Location = New System.Drawing.Point(278, 38)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(168, 25)
+        Me.lblHeading.TabIndex = 9
+        Me.lblHeading.Text = "Latte Selection"
+        '
+        'frmLatteSelection
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(714, 661)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lblConfirmation)
+        Me.Controls.Add(Me.lblInstructions)
+        Me.Controls.Add(Me.btnMocha)
+        Me.Controls.Add(Me.btnSelect)
+        Me.Controls.Add(Me.btnPumpkin)
+        Me.Controls.Add(Me.picMocha)
+        Me.Controls.Add(Me.picPumpkin)
+        Me.Controls.Add(Me.lblHeading)
+        Me.Name = "frmLatteSelection"
+        Me.Text = "Latte Selection"
+        CType(Me.picMocha, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picPumpkin, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblConfirmation As Label
+    Friend WithEvents lblInstructions As Label
+    Friend WithEvents btnMocha As Button
+    Friend WithEvents btnSelect As Button
+    Friend WithEvents btnPumpkin As Button
+    Friend WithEvents picMocha As PictureBox
+    Friend WithEvents picPumpkin As PictureBox
+    Friend WithEvents lblHeading As Label
+End Class
---
+++ Bulmer Latte/frmLatteSelection.vb
@@ -0,0 +1,22 @@
+﻿' Program Name: Latte Selection'
+' Developer:    Mark Bulmer
+' Purpose:      Choose the best latte.
+Public Class frmLatteSelection
+    Private Sub btnPumpkin_Click(sender As Object, e As EventArgs) Handles btnPumpkin.Click
+        ' This code is executed when the user clicks the Pumpkin Spice button. It displays the
+        ' pumpkin picture, hides the mocha picture, and enables the Select Latte button.
+
+        picPumpkin.Visible = True
+        picMocha.Visible = False
+        btnSelect.Enabled = True
+    End Sub
+
+    Private Sub btnMocha_Click(sender As Object, e As EventArgs) Handles btnMocha.Click
+        ' This code is executed when the user clicks the Mocha button. It displays the
+        ' mocha picture, hides the pumpkin picture, and enables the Select Latte button.
+
+        picPumpkin.Visible = False
+        picMocha.Visible = True
+        btnSelect.Enabled = True
+    End Sub
+End Class
